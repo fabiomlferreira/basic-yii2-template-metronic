2be6d590bc7b65baab8908fc60085967f465310c
Fábio Ferreira <eu@fabioferreira.pt>
2020-01-02 16:53:55 +0000

    Update and rename db.php to db_example.php

--- config/db_example.php
+++ config/db_example.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dev_gestao1',
-    'username' => 'dev_gestao1',
-    'password' => 'oyeFoQAKu4qB',
+    'dsn' => 'mysql:host=localhost;dbname=dbname',
+    'username' => 'dbusername',
+    'password' => 'password',
     'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
